f269aebd2a97916125f68c23600e00381b6eb052
Catarinacg16 <a85266@alunos.uminho.pt>
2020-03-08 11:03:44 +0000

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace trabalhoLI4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
